038903bf2dd08e6a34c370fa50990eaf834b8077
Yana <leduschenkova6@gmail.com>
2022-10-13 14:19:31 +0300

    Add Task 69

---
+++ Task69/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 69. Напишите программу, которая принимает на вход два числа А и В, и возводит число А в степень В 
+// с помощью рекурсии.
+// А = 3; В = 5; -> 243 
+// A = 2, B = 3 -> 8
+
+Console.Write("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int NumberExponent(int a, int b)
+{
+    if (b == 1) return a;
+    else return a * NumberExponent(a, b - 1);
+}
+
+int result = NumberExponent(A, B);
+Console.WriteLine(result);
